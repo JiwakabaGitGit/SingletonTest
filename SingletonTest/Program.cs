dc51ee72c5d7e82f8bdf7fc4154629e0cf147476
JiwakabaGitGit <jiwakaba.blog@gmail.com>
2021-10-10 17:41:30 +0900

    動作確認用のコード追加

---
+++ SingletonTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace Singletontest
+{
+    class Program
+    {
+        static void Main( string [ ] args )
+        {
+            // [追加]初期化。ここでコンストラクタを呼ぶことでシングルトンのオブジェクトが作成される
+            Console.WriteLine( "Main Initialize Start" );
+            Singleton.Instance.Initialize();
+            Console.WriteLine( "Main Initialize End" );
+
+            Thread thread = new Thread( new ThreadStart( () =>
+            {
+                thread_func();
+            } ) );
+
+            thread.Start();
+
+            Console.WriteLine( "Main InstanceName Start" );
+
+            // スリーブしてスレッド側でコンストラクタを呼ぶ
+            Thread.Sleep( 100 );
+
+            string str = Singleton.Instance.getInstanceName();
+            Console.WriteLine( "Main InstanceName:{0}", str );
+            Console.WriteLine( "Main InstanceName End" );
+
+            thread.Join();
+        }
+
+        static void thread_func()
+        {
+            Console.WriteLine( "Thread Start" );
+
+            string str = Singleton.Instance.getInstanceName();
+            Console.WriteLine( "Thread Instance Name:{0}", str );
+
+            Console.WriteLine( "Thread Sleep Start" );
+            Thread.Sleep( 3000 );
+            Console.WriteLine( "Thread Sleep End" );
+
+            Console.WriteLine( "Thread End" );
+        }
+    }
+}
---
+++ SingletonTest/Singleton.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Singletontest
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+
+        private static readonly object _syncRoot = new object();
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Console.WriteLine( "get start Instance" );
+
+                lock ( _syncRoot )
+                {
+                    if ( _instance == null )
+                    {
+                        Console.WriteLine( "初期インスタンス作成につき名前を入力してください" );
+                        var str = Console.ReadLine();
+
+                        _instance = new Singleton( str );
+                    }
+                    else
+                    {
+                        Console.WriteLine( "Instance:Exist" );
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private string m_instanceName;
+
+        private Singleton( string name) : this()
+        {
+            m_instanceName = name;
+            Console.WriteLine( "Singleton:{0}", m_instanceName );
+        }
+
+        private Singleton()
+        {
+            m_instanceName = nameof( Singleton );
+        }
+
+        ~Singleton()
+        {
+            Console.WriteLine( "Del" );
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public string getInstanceName()
+        {
+            Console.WriteLine( "getInstanceeName:{0}", m_instanceName );
+            return m_instanceName;
+        }
+    }
+}
